f8057ef7204fa99e4c8cedf078b4f47349788069
Charijoy Cempron <134136959+cha-ryne@users.noreply.github.com>
2025-01-09 16:58:40 +0800

    try pinkbg

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,11 +1,10 @@
-
-  const app = Vue.createApp({
-   data() {
-    return {
-     message: "Hello World!"
-    }
+const app = Vue.createApp({
+  data() {
+   return {
+    message: "Hello WEBPROG MI231 World!",
+    vueClass: "pinkBG"
    }
-  })
-  
-  app.mount('#app')
-
+  }
+ })
+ 
+ app.mount('#app')
