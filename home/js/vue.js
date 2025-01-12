02e4cc1c607c996b77391292c54f9c131c8a9cb5
Charijoy Cempron <134136959+cha-ryne@users.noreply.github.com>
2025-01-12 23:45:43 +0800

    h

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,10 +1,11 @@
-     const app = Vue.createApp({
-      data() {
-       return {
-        message: "Hello World!",
-        vueClass: "pinkBG"
-       }
-     }
-    })
- 
-    app.mount('#app')
+const app = Vue.createApp({
+  data() {
+    return {
+      message: "Hello World!",
+      vueClass: "pinkBG",
+      randomNumber: Math.ceil(Math.random() * 6)
+    };
+  }
+});
+
+app.mount("#app");
