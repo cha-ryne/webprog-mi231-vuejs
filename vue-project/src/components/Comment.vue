c58452e9f2ac11bd315cc0f963c8167ebb5ba5f4
Charijoy Cempron <134136959+cha-ryne@users.noreply.github.com>
2025-02-05 15:50:37 +0000

    try

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -1,46 +1,52 @@
 <template>
+  <div>
     <h1>Comments</h1>
     <ul>
-      <li v-for="comment in comments" :key="comment.id">{{ comment.name }} {{ comment.comment }}</li>
+      <li v-for="comment in comments" :key="comment.id" class="comment-item">
+        <span class="comment-name">{{ comment.name }}</span>: 
+        <span class="comment-text">{{ comment.comment }}</span>
+      </li>
     </ul>
-  </template>
-  
-  <script></script>
-  
-  <style>
-    #app > div {
-      border: dashed black 1px;
-      display: inline-block;
-      margin: 10px;
-      padding: 10px;
-      background-color: lightyellow;
-    }
-  </style>
+  </div>
+</template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from '../lib/supabaseClient'
+import { ref, onMounted } from 'vue';
+import { supabase } from '../lib/supabaseClient';
 
-const comments = ref([])
+const comments = ref([]);
 
 async function getComments() {
-  const { data } = await supabase.from('comments').select()
-  comments.value = data
+  const { data } = await supabase.from('comments').select();
+  comments.value = data;
 }
 
 onMounted(() => {
-  getComments()
-})
-
+  getComments();
+});
 </script>
 
+<style scoped>
+.comment-item {
+  border-bottom: 1px solid #ccc;
+  padding: 10px 0;
+}
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightyellow;
-  }
+.comment-name {
+  font-weight: bold;
+  color: #333;
+}
+
+.comment-text {
+  margin-left: 5px;
+  color: #555;
+}
+
+#app > div {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightyellow;
+}
 </style>
